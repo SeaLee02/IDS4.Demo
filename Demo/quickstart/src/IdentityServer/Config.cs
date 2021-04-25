6683563a2e8031ecfb3fae03114b5128ec9cd004
sealee <654029739@qq.com>
2021-04-25 11:31:02 +0800

    测试

---
+++ Demo/quickstart/src/IdentityServer/Config.cs
@@ -0,0 +1,90 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+using IdentityModel;
+using IdentityServer4;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace IdentityServer
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new IdentityResource[]
+            {
+                new IdentityResources.OpenId(),
+                  new IdentityResources.Profile(),
+            };
+
+        /// <summary>
+        /// 定义API范围
+        /// </summary>
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+            {
+             new ApiScope("api1", "My API")
+            };
+
+        /// <summary>
+        /// 定义客户端
+        /// </summary>
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+            {
+                new Client
+                    {
+                        ClientId = "client",
+
+                        // no interactive user, use the clientid/secret for authentication
+                        AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                        // secret for authentication
+                        ClientSecrets =
+                        {
+                            new Secret("secret".Sha256())
+                        },
+
+                        // scopes that client has access to
+                        AllowedScopes = { "api1" }
+                    },
+                 /// 资源所有者密码凭证（ResourceOwnerPassword）
+                ///     Resource Owner其实就是User，所以可以直译为用户名密码模式。
+                ///     密码模式相较于客户端凭证模式，多了一个参与者，就是User。
+                ///     通过User的用户名和密码向Identity Server申请访问令牌。
+                new Client
+                {
+                    ClientId = "client1",
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+                    ClientSecrets = { new Secret("secret".Sha256()) },
+                    AllowedScopes = { "api1" }
+                },           
+                /// 授权码模式（Code）
+                ///     适用于保密客户端（Confidential Client），比如ASP.NET MVC等服务器端渲染的Web应用
+                   // interactive ASP.NET Core MVC client
+                    new Client
+                    {
+                        ClientId = "mvc",
+                        ClientSecrets = { new Secret("secret".Sha256()) },
+
+                        AllowedGrantTypes = GrantTypes.Code,
+
+                        // where to redirect to after login 
+                        RedirectUris = { "http://localhost:5002/signin-oidc" },
+
+                        // where to redirect to after logout
+                        PostLogoutRedirectUris = { "http://localhost:5002/signout-callback-oidc" },
+
+                        AllowedScopes = new List<string>
+                        {
+                            IdentityServerConstants.StandardScopes.OpenId,
+                            IdentityServerConstants.StandardScopes.Profile
+                        }
+                    }
+            };
+
+
+    }
+}
